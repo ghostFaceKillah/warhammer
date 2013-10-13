1a91e70d757056df02595cadb909b067f3d6f4b1
michal_garmulewicz <garmulewicz@gmail.com>
2013-10-13 16:18:31 +0200

    small bit of code growth

--- main.cpp
+++ main.cpp
@@ -2,56 +2,89 @@
 #include <ctime>
 #include <iostream>
 #include <vector>
+#include <string>
+#include <map>
 
 using namespace std;
 
-int irand(int min, int max) {
-  return ((double)rand() / ((double)RAND_MAX + 1.0)) * (max - min + 1) + min;
-}
+int irand(int min, int max) { return ((double)rand() / ((double)RAND_MAX + 1.0)) * (max - min + 1) + min;}
+
+int toHit( int attackerWS, int defendantWS) {
+  if ( attackerWS > defendantWS ) { return 3; } 
+  else { if (attackerWS*2 < defendantWS) { return 5; } else { return 4; } };
+};
 
 class TrooperType {
 public:
-  int WS;
-  int BS;
-  int W;
-  int T;
-  int Ld;
-  TrooperType() { WS = 1; BS =1; W=1; T=3;  Ld = 7; }
-  TrooperType(int WS, int BS, int W, int T, int Ld) : WS(WS), BS(BS), W(W), T(T), Ld(Ld) { }
+  string name; int WS; int BS; int W; int T; int Ld;
+  TrooperType() { name = "just a squig";  WS = 1; BS =1; W=1; T=3;  Ld = 7; }
+  TrooperType(string name, int WS, int BS, int W, int T, int Ld) : name(name),  WS(WS), BS(BS), W(W), T(T), Ld(Ld) { }
 };
 
 class Unit { 
 public:
-  TrooperType type;
-  int size;
-  int orginalSize;
+  TrooperType type; int size; int orginalSize;
 };
 
-class dice {
+class Dice {
 public:
-  int k(int i) { return irand(1,i); }
+  int k(int i) { return irand(1,i); };
+  std::vector<int> rzuc_n_razy_k(int howMuch, int range) {
+    std::vector<int> resu;
+    for (int s = 0; s<howMuch; ++s ) {  resu.push_back(irand(1,range)); };
+    return resu;
+   };
 };
 
-void declareTroopers(std::vector<TrooperType>* troopers) {
-  
-  // troopers->push_back(TrooperType(3,3,4,1,7)); // one way 
-  
-  TrooperType Elf;
-  Elf.WS = 4;
-  Elf.T = 3;
-  Elf.W = 1;
-  Elf.Ld = 9;
-  troopers->push_back(Elf);
+class Fight {
+  protected:
+    Dice rzuc;
+    Unit Adversary1, Adversary2;
+  public:
+    Fight() { Dice rzut; Unit adv1, adv2;   rzuc = rzut; Adversary1 = adv1; Adversary2 = adv2; };
+    Fight( Unit adv1, Unit adv2 ) : Adversary1(adv1), Adversary2(adv2)  {  };
+    void resolveFightingTurn(Unit &adversary1, Unit &adversary2, Dice rzut);
+};
+
+void Fight::resolveFightingTurn(Unit &adversary1, Unit &adversary2, Dice rzut) {
+  cout << "Pierwszy walczący to " << adversary1.type.name << ", i ma on WS-a " << adversary1.type.WS << "," << endl;
+  cout << "a drugi to " << adversary2.type.name << ", i ma on WS-a " << adversary2.type.WS << "." << endl;
+  int hitRoll = rzut.k(6);
+  int needToHit = toHit( adversary1.type.WS, adversary2.type.WS);
+  cout << "Na hita musi wyrzucić " << needToHit  << " a wyrzucił "  << hitRoll << endl;
+  if (hitRoll >= needToHit) { cout << "trafił" << endl; } else { cout << " nie trafił " << endl; };
+
+}
+
+void declareTroopers(map<string,TrooperType>* troopers) {
+  // WS BS W T Ld
+
+  (*troopers)["Elf"] = TrooperType("Elf",4,4,1,3,9);
+  (*troopers)["Orc"] = TrooperType("Orc",3,3,1,4,8);
 };
 
 int main() {
-  // init 
+
   srand(time(0));
-  dice rzuc;
-  
-  std::vector<TrooperType> troopers; 
+  Dice kostka;
+  map<string,TrooperType> troopers; 
   declareTroopers(&troopers);
-  cout << troopers[0].W << endl;
-  // some action
+
+  Unit OrcGroup;
+  OrcGroup.type = troopers["Orc"];
+  OrcGroup.size =1;
+  Unit ElfGroup;
+  ElfGroup.type = troopers["Elf"];
+  ElfGroup.size =1;
+
+  Fight melee;
+  // melee.resolveFightingTurn(OrcGroup, ElfGroup, kostka);
+  
+  std::vector<int> porcys;
+  porcys = kostka.rzuc_n_razy_k(10,6);
+  for (int log = 0; log < porcys.size(); ++log ) {
+  cout << porcys[log] << endl; };
+
+
 
 }
